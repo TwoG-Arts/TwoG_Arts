c66f381cce1088c09dd21461832826fd88c5fe62
GiacchettiLuigi <81223417+Rankoll@users.noreply.github.com>
2022-09-14 10:50:05 +0200

    Fixing language script.

--- js/pages.js
+++ js/pages.js
@@ -95,8 +95,8 @@ function select(i) {
 function language() {
     let toTranslate = document.getElementsByClassName('en');
     const wrap = document.getElementsByClassName('wrap')[0];
-    const design = document.getElementsByClassName('video')[0];
-    const editing = document.getElementsByClassName('works')[0];
+    const design = document.getElementById('design');
+    const editing = document.getElementById('editing');
 
     if(document.getElementsByClassName('check')[0].checked) {
         toTranslate[0].textContent = 'About Me';
@@ -108,7 +108,7 @@ function language() {
         toTranslate[6].textContent = 'More videos';
         toTranslate[7].textContent = 'More designs';
         toTranslate[8].textContent = 'Contacts';
-        if (wrap.style.display === 'flex') {
+        if (wrap.hasAttribute('flex')) {
             toTranslate[9].textContent = 'Contacts';
             toTranslate[10].textContent = 'Collaborations';
             toTranslate[11].textContent = 'Special thanks';
@@ -121,12 +121,12 @@ function language() {
             toTranslate[18].textContent = 'More about';
             toTranslate[19].textContent = 'More about';
         }
-        if (design.style.display === 'flex') {
+        if (design.hasAttribute('flex')) {
             toTranslate[9].textContent = 'Graphics and logo for RaamDeadman\'s socials';
             toTranslate[10].textContent = 'Graphics and logo for the 2nd Youtube channel of RaamDeadman';
             toTranslate[11].textContent = 'Graphics and logo for my socials';
         }
-        if (editing.style.display === 'flex') {
+        if (editing.hasAttribute('flex')) {
             toTranslate[9].textContent = 'Basim Ibn Ishaq da Assassin\'s Creed by RaamDeadman';
             toTranslate[10].textContent = 'The Rumbling from Attack on Titan.';
             toTranslate[11].textContent = 'Connor from Detroit Become Human by RaamDeadman.';
@@ -146,7 +146,7 @@ function language() {
         toTranslate[6].textContent = 'Più video';
         toTranslate[7].textContent = 'Più designs';
         toTranslate[8].textContent = 'Contatti';
-        if (wrap.style.display === 'flex') {
+        if (wrap.hasAttribute('flex')) {
             toTranslate[9].textContent = 'Contatti';
             toTranslate[10].textContent = 'Collaborazioni';
             toTranslate[11].textContent = 'Ringraziamenti';
@@ -159,12 +159,12 @@ function language() {
             toTranslate[18].textContent = 'Scopri di più';
             toTranslate[19].textContent = 'Scopri di più';
         }
-        if (design.style.display === 'flex') {
+        if (design.hasAttribute('flex')) {
             toTranslate[9].textContent = 'Grafiche e logo per i social di RaamDeadman';
             toTranslate[10].textContent = 'Grafiche e logo per il secondo canale youtube di RaamDeadman';
             toTranslate[11].textContent = 'Grafiche e logo per i miei social';
         }
-        if (editing.style.display === 'flex') {
+        if (editing.hasAttribute('flex')) {
             toTranslate[9].textContent = 'Basim Ibn Ishaq da Assassin\'s Creed by RaamDeadman';
             toTranslate[10].textContent = 'Il Boato da L\'attacco dei Giganti.';
             toTranslate[11].textContent = 'Connor da Detroit Become Human by RaamDeadman.';

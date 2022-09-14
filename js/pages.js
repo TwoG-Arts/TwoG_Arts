7c1bce32b2957d87a71d0194e48f7852947972f9
GiacchettiLuigi <81223417+Rankoll@users.noreply.github.com>
2022-09-14 11:31:19 +0200

    Fixing iterator values in javascript function language.

--- js/pages.js
+++ js/pages.js
@@ -96,19 +96,18 @@ function makeRangeIterator(start = 0, end = Infinity, step = 1) {
     let nextIndex = start;
     let iterationCount = 0;
 
-    const rangeIterator = {
+    return {
         next() {
             let result;
             if (nextIndex < end) {
-                result = { value: nextIndex, done: false };
+                result = {value: nextIndex, done: false};
                 nextIndex += step;
                 iterationCount++;
                 return result;
             }
-            return { value: iterationCount, done: true };
+            return {value: iterationCount, done: true};
         }
     };
-    return rangeIterator;
 }
 
 function language() {
@@ -119,81 +118,81 @@ function language() {
     const it = makeRangeIterator(0, 150, 1);
 
     if(document.getElementsByClassName('check')[0].checked) {
-        toTranslate[it.next()].textContent = 'About Me';
-        toTranslate[it.next()].textContent = 'Videos';
-        toTranslate[it.next()].textContent = 'About Me';
-        toTranslate[it.next()].textContent = 'Works';
-        toTranslate[it.next()].textContent = 'Works';
-        toTranslate[it.next()].textContent = 'More edits';
-        toTranslate[it.next()].textContent = 'More videos';
-        toTranslate[it.next()].textContent = 'More designs';
-        toTranslate[it.next()].textContent = 'Contacts';
+        toTranslate[it.next().value].textContent = 'About Me';
+        toTranslate[it.next().value].textContent = 'Videos';
+        toTranslate[it.next().value].textContent = 'About Me';
+        toTranslate[it.next().value].textContent = 'Works';
+        toTranslate[it.next().value].textContent = 'Works';
+        toTranslate[it.next().value].textContent = 'More edits';
+        toTranslate[it.next().value].textContent = 'More videos';
+        toTranslate[it.next().value].textContent = 'More designs';
+        toTranslate[it.next().value].textContent = 'Contacts';
         if (wrap) {
-            toTranslate[it.next()].textContent = 'Contacts';
-            toTranslate[it.next()].textContent = 'Collaborations';
-            toTranslate[it.next()].textContent = 'Special thanks';
-            toTranslate[it.next()].textContent = 'Works';
-            toTranslate[it.next()].textContent = 'My name is Luigi but you can call me Giorgio';
-            toTranslate[it.next()].textContent = 'Lately I am working with';
-            toTranslate[it.next()].textContent = 'Collaborations';
-            toTranslate[it.next()].textContent = 'Special thanks to Rankoll for coding, myself for every detailed illustrations';
-            toTranslate[it.next()].textContent = 'More about';
-            toTranslate[it.next()].textContent = 'More about';
-            toTranslate[it.next()].textContent = 'More about';
+            toTranslate[it.next().value].textContent = 'Contacts';
+            toTranslate[it.next().value].textContent = 'Collaborations';
+            toTranslate[it.next().value].textContent = 'Special thanks';
+            toTranslate[it.next().value].textContent = 'Works';
+            toTranslate[it.next().value].textContent = 'My name is Luigi but you can call me Giorgio';
+            toTranslate[it.next().value].textContent = 'Lately I am working with';
+            toTranslate[it.next().value].textContent = 'Collaborations';
+            toTranslate[it.next().value].textContent = 'Special thanks to Rankoll for coding, myself for every detailed illustrations';
+            toTranslate[it.next().value].textContent = 'More about';
+            toTranslate[it.next().value].textContent = 'More about';
+            toTranslate[it.next().value].textContent = 'More about';
         }
         if (design) {
-            /*toTranslate[it.next()].textContent = 'Graphics very cool';*/
-            toTranslate[it.next()].textContent = 'Graphics and logo for RaamDeadman\'s socials';
-            toTranslate[it.next()].textContent = 'Graphics and logo for the 2nd Youtube channel of RaamDeadman';
-            toTranslate[it.next()].textContent = 'Graphics and logo for my socials';
+            /*toTranslate[it.next().value].textContent = 'Graphics very cool';*/
+            toTranslate[it.next().value].textContent = 'Graphics and logo for RaamDeadman\'s socials';
+            toTranslate[it.next().value].textContent = 'Graphics and logo for the 2nd Youtube channel of RaamDeadman';
+            toTranslate[it.next().value].textContent = 'Graphics and logo for my socials';
         }
         if (editing) {
-            toTranslate[it.next()].textContent = 'Basim Ibn Ishaq from Assassin\'s Creed by RaamDeadman';
-            toTranslate[it.next()].textContent = 'The Rumbling from Attack on Titan.';
-            toTranslate[it.next()].textContent = 'Connor from Detroit Become Human by RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Okabe & Daru from Steins;Gate w/RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Shanks from One Piece by RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Thor Ravager from Thor Love&Thunder by RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Aiden Pearce from Watch Dogs.';
-            toTranslate[it.next()].textContent = 'Soul from Dark Soul.';
+            toTranslate[it.next().value].textContent = 'Basim Ibn Ishaq from Assassin\'s Creed by RaamDeadman';
+            toTranslate[it.next().value].textContent = 'The Rumbling from Attack on Titan.';
+            toTranslate[it.next().value].textContent = 'Connor from Detroit Become Human by RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Okabe & Daru from Steins;Gate w/RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Shanks from One Piece by RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Thor Ravager from Thor Love&Thunder by RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Aiden Pearce from Watch Dogs.';
+            toTranslate[it.next().value].textContent = 'Soul from Dark Soul.';
         }
     } else {
-        toTranslate[it.next()].textContent = 'Chi sono';
-        toTranslate[it.next()].textContent = 'Video';
-        toTranslate[it.next()].textContent = 'Chi sono';
-        toTranslate[it.next()].textContent = 'Lavori';
-        toTranslate[it.next()].textContent = 'Lavori';
-        toTranslate[it.next()].textContent = 'Più editing';
-        toTranslate[it.next()].textContent = 'Più video';
-        toTranslate[it.next()].textContent = 'Più designs';
-        toTranslate[it.next()].textContent = 'Contatti';
+        toTranslate[it.next().value].textContent = 'Chi sono';
+        toTranslate[it.next().value].textContent = 'Video';
+        toTranslate[it.next().value].textContent = 'Chi sono';
+        toTranslate[it.next().value].textContent = 'Lavori';
+        toTranslate[it.next().value].textContent = 'Lavori';
+        toTranslate[it.next().value].textContent = 'Più editing';
+        toTranslate[it.next().value].textContent = 'Più video';
+        toTranslate[it.next().value].textContent = 'Più designs';
+        toTranslate[it.next().value].textContent = 'Contatti';
         if (wrap) {
-            toTranslate[it.next()].textContent = 'Contatti';
-            toTranslate[it.next()].textContent = 'Collaborazioni';
-            toTranslate[it.next()].textContent = 'Ringraziamenti';
-            toTranslate[it.next()].textContent = 'Lavori';
-            toTranslate[it.next()].textContent = 'Il mio nome è Luigi, sono un tecnico informatico che ha iniziato a frequentare l\'accademia delle belle arti di Napoli presso la facoltà di Nuove Tecnologie dell’arte nel 2020. Il mio nome “2G” deriva dalla mia opera preferita: NieR Automata, gioco che mi ha segnato profondamente, grazie al suo scavare a fondo nella natura umana ed alla narrazione impeccabile del maestro Yoko Taro. Principalmente mi occupo di fotomontaggio e fotoritocco, mi diletto nella creazione di VFX e nel video editing.';
-            toTranslate[it.next()].textContent = 'Attualmente collaboro con il cosplayer e Youtuber RaamDeadman, a cui edito le foto e curo le estetiche dei social e lo streamer LosMichele, editando i momenti salienti delle sue live mediante diversi format, come i Best of e le Pillole. Sono un fruitore assiduo di anime, manga, videogames da cui prendo ispirazione per i miei lavori di fotomontaggio ed amo il cinema, passione che mi ha avvicinato al mondo dei VFX e dell’editing video in generale.';
-            toTranslate[it.next()].textContent = 'Collaborazioni';
-            toTranslate[it.next()].textContent = 'Un ringraziamento speciale a ciccio e caio';
-            toTranslate[it.next()].textContent = 'Scopri di più';
-            toTranslate[it.next()].textContent = 'Scopri di più';
-            toTranslate[it.next()].textContent = 'Scopri di più';
+            toTranslate[it.next().value].textContent = 'Contatti';
+            toTranslate[it.next().value].textContent = 'Collaborazioni';
+            toTranslate[it.next().value].textContent = 'Ringraziamenti';
+            toTranslate[it.next().value].textContent = 'Lavori';
+            toTranslate[it.next().value].textContent = 'Il mio nome è Luigi, sono un tecnico informatico che ha iniziato a frequentare l\'accademia delle belle arti di Napoli presso la facoltà di Nuove Tecnologie dell’arte nel 2020. Il mio nome “2G” deriva dalla mia opera preferita: NieR Automata, gioco che mi ha segnato profondamente, grazie al suo scavare a fondo nella natura umana ed alla narrazione impeccabile del maestro Yoko Taro. Principalmente mi occupo di fotomontaggio e fotoritocco, mi diletto nella creazione di VFX e nel video editing.';
+            toTranslate[it.next().value].textContent = 'Attualmente collaboro con il cosplayer e Youtuber RaamDeadman, a cui edito le foto e curo le estetiche dei social e lo streamer LosMichele, editando i momenti salienti delle sue live mediante diversi format, come i Best of e le Pillole. Sono un fruitore assiduo di anime, manga, videogames da cui prendo ispirazione per i miei lavori di fotomontaggio ed amo il cinema, passione che mi ha avvicinato al mondo dei VFX e dell’editing video in generale.';
+            toTranslate[it.next().value].textContent = 'Collaborazioni';
+            toTranslate[it.next().value].textContent = 'Un ringraziamento speciale a ciccio e caio';
+            toTranslate[it.next().value].textContent = 'Scopri di più';
+            toTranslate[it.next().value].textContent = 'Scopri di più';
+            toTranslate[it.next().value].textContent = 'Scopri di più';
         }
         if (design) {
-            toTranslate[it.next()].textContent = 'Grafiche e logo per i social di RaamDeadman';
-            toTranslate[it.next()].textContent = 'Grafiche e logo per il secondo canale youtube di RaamDeadman';
-            toTranslate[it.next()].textContent = 'Grafiche e logo per i miei social';
+            toTranslate[it.next().value].textContent = 'Grafiche e logo per i social di RaamDeadman';
+            toTranslate[it.next().value].textContent = 'Grafiche e logo per il secondo canale youtube di RaamDeadman';
+            toTranslate[it.next().value].textContent = 'Grafiche e logo per i miei social';
         }
         if (editing) {
-            toTranslate[it.next()].textContent = 'Basim Ibn Ishaq da Assassin\'s Creed by RaamDeadman';
-            toTranslate[it.next()].textContent = 'Il Boato da L\'attacco dei Giganti.';
-            toTranslate[it.next()].textContent = 'Connor da Detroit Become Human by RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Okabe & Daru da Steins;Gate w/RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Shanks da One Piece by RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Thor Ravager da Thor Love&Thunder by RaamDeadman.';
-            toTranslate[it.next()].textContent = 'Aiden Pearce da Watch Dogs.';
-            toTranslate[it.next()].textContent = 'Anima da Dark Soul.';
+            toTranslate[it.next().value].textContent = 'Basim Ibn Ishaq da Assassin\'s Creed by RaamDeadman';
+            toTranslate[it.next().value].textContent = 'Il Boato da L\'attacco dei Giganti.';
+            toTranslate[it.next().value].textContent = 'Connor da Detroit Become Human by RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Okabe & Daru da Steins;Gate w/RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Shanks da One Piece by RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Thor Ravager da Thor Love&Thunder by RaamDeadman.';
+            toTranslate[it.next().value].textContent = 'Aiden Pearce da Watch Dogs.';
+            toTranslate[it.next().value].textContent = 'Anima da Dark Soul.';
         }
     }
 }
